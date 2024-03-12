6f4a983474d8387a08a023fbe47d9acc6dfa6b8b
Ben Witman <benwit@microsoft.com>
2024-03-12 13:08:33 -0400

    add dominorerun* scripts

---
+++ tests/dominorerunmeta.bat
@@ -0,0 +1,9 @@
+setlocal
+@echo off
+IF "%1" == "" ((echo Usage: dominorerunmeta [testname]) & exit /B 1)
+
+call %~dp0_vars.bat %1
+IF ERRORLEVEL 1 exit /B %ERRORLEVEL%
+
+call %~dp0_metarun.bat
+IF ERRORLEVEL 1 exit /B %ERRORLEVEL%
---
+++ tests/dominorerunproduct.bat
@@ -0,0 +1,9 @@
+setlocal
+@echo off
+IF "%1" == "" ((echo Usage: dominorerunproduct [testname]) & exit /B 1)
+
+call %~dp0_vars.bat %1
+IF ERRORLEVEL 1 exit /B %ERRORLEVEL%
+
+call %~dp0_productrun.bat
+IF ERRORLEVEL 1 exit /B %ERRORLEVEL%
