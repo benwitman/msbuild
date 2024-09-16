248f853e3d010b2122c28d4e42c88c8b65cb5357
Ben Witman <benwit@microsoft.com>
2024-09-16 11:32:51 -0400

    Fix path to msbuild after vars update

--- tests/_msbuild.bat
+++ tests/_msbuild.bat
@@ -4,4 +4,4 @@ if NOT DEFINED _Static_Vars_Set ((echo Must run _vars.bat first) & exit /B 1)
 
 for /F %%i in ('dir /B %_Static_Source%\*proj') do (set _Static_Source_Project=%_Static_Source%\%%i)
 
-%_Static_Artifacts_MsBuild%\MSBuild.exe %_Static_MsBuild_Verbosity% %_Static_MsBuild_Args% %MSBUILDARGS% %_Static_Source_Project%
+%_Static_Artifacts_MsBuild% %_Static_MsBuild_Verbosity% %_Static_MsBuild_Args% %MSBUILDARGS% %_Static_Source_Project%
